f637167f90e96282e8341ded90a17be7e8bd6475
Imagine Rabbits <keerati.jear@gmail.com>
2022-07-22 21:08:43 +0700

    Fix to two products

--- cypress/e2e/cart-summary.cy.js
+++ cypress/e2e/cart-summary.cy.js
@@ -5,32 +5,34 @@ describe("Cart Summary", () => {
     Cypress.session.clearAllSavedSessions();
   });
 
-  const addOneProduct = () => {
+  const addTwoProducts = () => {
     customer.visit("/");
 
     customer.get(':nth-child(1) > .space-x-2 > [data-cy="product-item--add-to-cart"]')
       .click();
+    customer.get(':nth-child(1) > .space-x-2 > [data-cy="product-item--add-to-cart"]')
+      .click();
   }
 
-  it("Display Cart Summary", () => {
-    addOneProduct();
+  it("Display cart summary with two products", () => {
+    addTwoProducts();
 
     customer.visit("/cart/summary");
 
     customer.get('a > .mb-2')
       .should("have.text", "Loose Cropped Jeans (Damaged)");
     customer.get('.relative > .w-full')
-      .should("have.value", "1");
+      .should("have.value", "2");
     customer.get('.hidden > .text-sm')
       .should("have.text", "$42.57");
     customer.get(':nth-child(4) > .text-sm')
-      .should("have.text", "$42.57");
+      .should("have.text", "$85.14");
 
     customer.get(':nth-child(1) > .text-gray-900')
-      .should("have.text", " $42.57");
+      .should("have.text", " $85.14");
     customer.get(':nth-child(2) > .text-gray-900')
       .should("have.text", " -$0.00");
     customer.get(':nth-child(3) > .text-gray-900')
-      .should("have.text", " $42.57");
+      .should("have.text", " $85.14");
   });
 });
