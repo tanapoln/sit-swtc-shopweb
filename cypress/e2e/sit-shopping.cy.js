cc97799e280fd8cb969e3a1f5094caf1fb9547f7
bazsup <bas@odds.team>
2022-07-29 15:03:19 +0700

    ✅ Parameterized SS-14

--- cypress/e2e/sit-shopping.cy.js
+++ cypress/e2e/sit-shopping.cy.js
@@ -1,15 +1,27 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("SIT Shopping", () => {
-  it("SS-14: a user with a single item checkout", () => {
-    cy.visit("/");
+  const ss14TestData = [
+    {
+      product: "Printed Cotton Square Neck Short Sleeve Mini Dress",
+      totalPrice: "$28.28",
+    },
+    {
+      product: "Printed Button Down Camisole Flare Dress",
+      totalPrice: "$42.57",
+    },
+  ];
+  ss14TestData.forEach((testdata) => {
+    it(`SS-14: a user with a single item (${testdata.product}) checkout`, () => {
+      cy.visit("/");
 
-    cy.addProductToCart("Printed Cotton Square Neck Short Sleeve Mini Dress");
+      cy.addProductToCart(testdata.product);
 
-    cy.visit("/cart/summary");
+      cy.visit("/cart/summary");
 
-    cy.get('[data-cy="subtotal"]').shouldHaveText("$28.28");
-    cy.get('[data-cy="discount-amount"]').shouldHaveText("-$0.00");
-    cy.get('[data-cy="total"]').shouldHaveText("$28.28");
+      cy.get('[data-cy="subtotal"]').shouldHaveText(testdata.totalPrice);
+      cy.get('[data-cy="discount-amount"]').shouldHaveText("-$0.00");
+      cy.get('[data-cy="total"]').shouldHaveText(testdata.totalPrice);
+    });
   });
 });
